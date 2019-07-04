81dd627ba360c4c5d06e9b672a628d847d017c82
samiraydi <samiraydi26@gmail.com>
2019-07-04 10:46:01 +0100

    Updates

---
+++ precache-manifest.032cc7404ad930872f374d6db7f44315.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89c7cfff4b4e927821321616f036696c",
+    "url": "/movi-react/index.html"
+  },
+  {
+    "revision": "1b1043297cecf903ab1f",
+    "url": "/movi-react/static/css/main.75a5e2d5.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/movi-react/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "1b1043297cecf903ab1f",
+    "url": "/movi-react/static/js/main.e85d9b01.chunk.js"
+  },
+  {
+    "revision": "c8da0e443de3b56c761a",
+    "url": "/movi-react/static/js/runtime~main.23bc4c0e.js"
+  }
+]);
